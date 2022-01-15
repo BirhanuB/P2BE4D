a1295b21a3795172ce37fb0b9b0ed2e9224d6cd2
BirhanuB <55416464+BirhanuB@users.noreply.github.com>
2022-01-15 00:10:44 -0500

    db updates

--- _helpers/db.js
+++ _helpers/db.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
-const connectionOptions = { useCreateIndex: true, useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false };
-mongoose.connect(process.env.MONGODB_URI || config.connectionString, connectionOptions, (err) => {
+mongoose.connect(process.env.MONGODB_URI || config.connectionString, (err) => {
     if (!err) { console.log('MongoDB connection successful!'); }
     else { console.log('Error in MongoDB connection : ' + JSON.stringify(err, undefined, 2))}
 });
